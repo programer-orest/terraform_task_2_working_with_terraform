1bc2a0273a8d7cde7f47c92a295c214115dc2197
Orest Yakymiv <akimivorest@gmail.com>
2025-06-18 19:56:15 +0300

    test

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "azurerm_storage_account" "example" {
 
 resource "azurerm_storage_container" "example" {
   name                  = var.container_name
-  storage_account_name    = azurerm_storage_account.example.name
+  storage_account_name  = azurerm_storage_account.example.name
   container_access_type = "private"
 }
 
